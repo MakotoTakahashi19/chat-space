e792404c8b58866e2c080b7f4fd2aabf4a9c25e7
MakotoTakahashi19 <makoto@macotsu-MacBookAir.local>
2019-04-26 15:19:24 +0900

    Create Asynchtonous communication

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,46 @@
+$(document).on('turbolinks:load',function(){
+  function buildHTML(message){
+    var html = `<div class="message">
+                  <div class="upper-message" id="post-scroll">
+                    <div class="message__user">
+                      ${ message.user_name }
+                    </div>
+                    <div class="message__date">
+                      ${ message.created_at }
+                    </div>
+                  </div>
+                  <div class="lower-message">
+                    <div class="message__text">
+                      ${ message.content}
+                    </div>
+                    <div class= "lower-message__image">
+                      ${ message.image = message.image || "" }
+                    </div>
+                  </div>
+                </div>`
+                return html;
+  }
+  $("#new_message").on("submit", function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr("action")
+    $.ajax({
+      url: url,
+      type: 'POST',
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false,
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('.textbox').val('');
+      $('#imagebox').val('');
+      $('.messages').animate({scrollTop: $(".messages")[0].scrollHeight}, "fast");
+    })
+    .fail(function(){
+      alert('文字もしくは画像を送信してください');
+    })
+  });
+});
