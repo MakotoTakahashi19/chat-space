ef31e35d76feeb3f881ea280d769d32545c48ae6
MakotoTakahashi19 <makoto@macotsu-MacBookAir.local>
2019-04-26 17:13:45 +0900

    delete unnecessary codes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  # before_action "users#index" if user_signed_in?
   root "groups#index"
   resources :users, only: [:index, :edit,:update] 
   resources :groups, only: [:new, :create, :edit, :update] do
