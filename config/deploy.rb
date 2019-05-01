ae53a6de59b34765b1733667844275a3b6c4e133
MakotoTakahashi19 <makoto@macotsu-MacBookAir.local>
2019-05-01 19:46:22 +0900

    update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "3.1"
+lock "3.10.0"
 
 set :application, "chat-space"
 set :repo_url, "git@github.com:MakotoTakahashi19/chat-space.git"
